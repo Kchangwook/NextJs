f782461e563d866fbcbef5917a0cc923eaeb2b53
kchangwook <kchangwook@naver.com>
2026-05-15 16:01:04 +0900

    [2.7] 글로벌 레이아웃 설정하기

---
+++ section02/src/components/global-layout.tsx
@@ -0,0 +1,17 @@
+import {ReactNode} from "react";
+import Link from "next/link";
+import style from "./global-layout.module.css";
+
+export default function GlobalLayout({children}: {children: ReactNode}) {
+    return (
+        <div className={style.container}>
+            <header>
+                <Link href={"/"}>📚 ONEBITE BOOKS</Link>
+            </header>
+            <main>
+                {children}
+            </main>
+            <footer>제작 @Changwook</footer>
+        </div>
+    )
+}
