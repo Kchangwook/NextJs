cefb0744ffb4c7839fe20d23912febf83ed59ba3
Kchangwook <16100210+Kchangwook@users.noreply.github.com>
2026-05-28 11:44:59 +0900

    [2.11] SSR 1

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,29 +1,22 @@
 // CSS Module
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import { ReactNode } from "react";
+import {ReactNode, useEffect} from "react";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import {InferGetServerSidePropsType} from "next";
 
-export default function Home() {
-  return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-      <section>
+export const getServiceSideProps = () => {
+    return {
+        props: {
+            data,
+        },
+    };
+};
+
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-    </div>
-  );
 }
 
 Home.getLayout = (page: ReactNode) => {
-  return <SearchableLayout>{page}</SearchableLayout>;
+    return <SearchableLayout>{page}</SearchableLayout>;
 };
