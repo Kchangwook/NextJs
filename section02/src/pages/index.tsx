9e772dd2ac6401ef21a1482d75966c7dec8a614f
Kchangwook <16100210+Kchangwook@users.noreply.github.com>
2026-05-28 11:45:11 +0900

    [2.11] SSR 1

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -14,7 +14,25 @@ export const getServiceSideProps = () => {
     };
 };
 
-        <h3>등록된 모든 도서</h3>
+export default function Home({data}: InferGetServerSidePropsType<typeof getServiceSideProps>) {
+    useEffect(() => console.log(window), []);
+
+    return (
+        <div className={style.container}>
+            <section>
+                <h3>지금 추천하는 도서</h3>
+                {books.map((book) => (
+                    <BookItem key={book.id} {...book} />
+                ))}
+            </section>
+            <section>
+                <h3>등록된 모든 도서</h3>
+                {books.map((book) => (
+                    <BookItem key={book.id} {...book} />
+                ))}
+            </section>
+        </div>
+    );
 }
 
 Home.getLayout = (page: ReactNode) => {
