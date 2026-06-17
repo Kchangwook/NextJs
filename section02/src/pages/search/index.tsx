dfa62cb312e7bf4b9bc15000bd520884bcb1c30f
Kchangwook <16100210+Kchangwook@users.noreply.github.com>
2026-06-17 11:21:22 +0900

    [2.19] SEO 설정하기

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,11 +1,10 @@
 import SearchableLayout from "@/components/searchable-layout";
 import {useRouter} from "next/router";
 import {ReactNode, useEffect, useState} from "react";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
-import {GetServerSidePropsContext, GetStaticPropsContext, InferGetStaticPropsType} from "next";
 import fetchBooks from "@/lib/fetch-books";
 import {BookData} from "@/types";
+import Head from "next/head";
 
 export default function Page() {
     const [searchedBooks, setSearchedBooks] = useState<BookData[]>([]);
@@ -25,11 +24,19 @@ export default function Page() {
     }, [q]);
 
     return (
-        <div>
-            {searchedBooks.map((book) => (
-                <BookItem key={book.id} {...book} />
-            ))}
-        </div>
+        <>
+            <Head>
+                <title>한입북스</title>
+                <meta property="og:image" content="/thumbnail.png" />
+                <meta property="og:title" content="한입북스 - 검색결과"/>
+                <meta property="og:description" content="한입 북스에 등록된 도서들을 만나보세요."/>
+            </Head>
+            <div>
+                {searchedBooks.map((book) => (
+                    <BookItem key={book.id} {...book} />
+                ))}
+            </div>
+        </>
     );
 }
 
