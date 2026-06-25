a4824fb123d2ffee6c4b609211002d64fe82ca89
Kchangwook <16100210+Kchangwook@users.noreply.github.com>
2026-06-25 16:16:05 +0900

    [3.2] 페이징 라우팅 시작하기

---
+++ section03/app/book/[id]/page.tsx
@@ -0,0 +1,6 @@
+export default async function BookPage({params}: { params: Promise<{ id: string }>}) {
+    const {id} = await params;
+    return <>
+        <div>Book 페이지: {id}</div>
+    </>
+}
---
+++ section03/app/search/page.tsx
@@ -0,0 +1,6 @@
+export default async function SearchPage({searchParams}: { searchParams: Promise<{ q: string }> }) {
+    const { q } = await searchParams;
+    return <>
+        <div>Search 페이지: {q}</div>
+    </>
+}
