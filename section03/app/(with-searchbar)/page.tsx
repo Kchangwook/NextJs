93864c7d11adae045fd5bc056d69c59141991ff2
Kchangwook <16100210+Kchangwook@users.noreply.github.com>
2026-06-28 17:04:25 +0900

    [3.4] 리액트 서버 컴포넌트

--- section03/app/(with-searchbar)/page.tsx
+++ section03/app/(with-searchbar)/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import Searchbar from "@/app/(with-searchbar)/searchbar";
 
 export default function Home() {
     return (
         <>
+            <Searchbar />
             <div>인덱스 페이지</div>
         </>
     );
