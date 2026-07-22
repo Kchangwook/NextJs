2f708d6c352bcd354bac5e31e54d141da793a68e
Kchangwook <16100210+Kchangwook@users.noreply.github.com>
2026-07-22 15:04:27 +0900

    [5.1] 풀 라우트 캐시

---
+++ section05/app/(with-searchbar)/layout.tsx
@@ -0,0 +1,16 @@
+import {ReactNode, Suspense} from "react";
+import Searchbar from "../../components/searchbar";
+
+export default function Layout(
+    {children}:
+    { children: ReactNode }
+) {
+    return (
+        <div>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Searchbar/>
+            </Suspense>
+            {children}
+        </div>
+    );
+}
