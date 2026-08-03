4d275bf011a1bfee0353bf7b0b83f36920d30ab1
Kchangwook <16100210+Kchangwook@users.noreply.github.com>
2026-08-03 16:27:08 +0900

    [5.2] 라우트 세그먼트 옵션

--- section05/app/(with-searchbar)/layout.tsx
+++ section05/app/(with-searchbar)/layout.tsx
@@ -7,6 +7,7 @@ export default function Layout(
 ) {
     return (
         <div>
+            <div>{new Date().toLocaleString()}</div>
             <Suspense fallback={<div>Loading...</div>}>
                 <Searchbar/>
             </Suspense>
